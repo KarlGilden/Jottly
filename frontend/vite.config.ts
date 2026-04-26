00db92748c0564e8e554d3221097bb35158a9ae4
KarlGilden <karl.gildenhuys@gmail.com>
2026-04-26 23:00:50 +1200

    update port

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,6 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
 	plugins: [react(), tailwindcss()],
 	server: {
-		port: 5173,
+		port: Number(process.env.PORT || 5173),
 	},
 });
