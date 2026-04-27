3cfd780b2285518d6bd69c7175d7cc03903dff71
KarlGilden <karl.gildenhuys@gmail.com>
2026-04-27 15:02:40 +1200

    add cors methods

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,6 +25,8 @@ const app = Fastify({
 await app.register(cors, {
 	origin: process.env.FRONTEND_URL || "http://localhost:5173",
 	credentials: true,
+	methods: ["GET", "PUT", "POST", "DELETE", "OPTIONS"],
+	allowedHeaders: ["Content-Type", "Authorization"],
 });
 
 await app.register(fastifyStatic, {
